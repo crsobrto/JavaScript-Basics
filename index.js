6774dfbf1ed6b7bdbe2aae84452580b51967a8fb
crsobrto <145484928+crsobrto@users.noreply.github.com>
2024-05-23 21:27:11 -0400

    Demonstrating variable declarations & initializations

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('Hello World'); // Statement in string form
+
+let name = 'Carson'; // Declaring a variable called "name"
+console.log(name);
+
+let firstName, lastName, middleName; // Declaring multiple variables on the same line
+
+let yourName = 'John', myName = 'Carson'; // Initializing multiple variables on the same line
