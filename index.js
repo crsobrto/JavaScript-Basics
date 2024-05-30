c4612a797e753d8e0031ab04bd57f2cf00cbf4a2
crsobrto <145484928+crsobrto@users.noreply.github.com>
2024-05-30 17:27:59 -0400

    Objects

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // JavaScript is a Dynamic Language, meaning that a variable's type can change during runtime
 // The console can be cleared with Ctrl+L
 // In JavaScript, all numbers are of type "number". There are no "integers", "floating points", "doubles", etc.
+// Bracket Notation allows an object's key-value pair to be selected at runtime by the user
+
+
 
 console.log('Hello World'); // Statement in string form
 
@@ -19,3 +22,30 @@ let age = 22; // Number Literal
 let isApproved = true; // Boolean Literal
 let firstNameTwo = undefined; // typeof is "undefined"
 let lastNameTwo = null; // null clears the value of a variable; typeof is "object"
+
+
+
+// Objects >
+
+let person = { // Object Literal
+    name: 'Carson', // key-value pair
+    age: 22 // key-value pair
+};
+
+console.log(person);
+
+// Dot Notation to change an object's key-value pair >
+person.name = 'Luke';
+console.log(person.name);
+// < Dot Notation to change an object's kay-value pair
+
+// Bracket Notation to change an object's key-value pair >
+let selection = 'name';
+person[selection] = 'Rick';
+console.log(person['name']);
+
+person['age'] = '15';
+console.log(person['age']);
+// < Bracket Notation to change an object's key-value pair
+
+// < Objects
