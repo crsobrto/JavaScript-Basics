d822081e7f6a3e38fa02b1db04c8063014c1b65e
crsobrto <145484928+crsobrto@users.noreply.github.com>
2024-05-30 17:04:05 -0400

    Constants

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ console.log(name);
 let firstName, lastName, middleName; // Declaring multiple variables on the same line
 
 let yourName = 'John', myName = 'Carson'; // Initializing multiple variables on the same line
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
