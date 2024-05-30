9c1782282c8be23c975a34e31ee5e55b65d8b59b
crsobrto <145484928+crsobrto@users.noreply.github.com>
2024-05-30 17:09:05 -0400

    Literals

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ let firstName, lastName, middleName; // Declaring multiple variables on the same
 let yourName = 'John', myName = 'Carson'; // Initializing multiple variables on the same line
 
 const interestRate = 0.3;
-interestRate = 1;
 console.log(interestRate);
+
+let nameTwo = 'Charlie'; // String Literal
+let age = '22'; // Number Literal
+let isApproved = true; // Boolean Literal
+let firstNameTwo = undefined; 
+let lastNameTwo = null; // null clears the value of a variable
